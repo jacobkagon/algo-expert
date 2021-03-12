3bbf30da5d51e7ee6c16ab8c6a8856200b7d245c
JakesProjects1 <jacob@kagon.net>
2021-03-12 14:40:53 -0500

    done with factorial problem

---
+++ factorial.js
@@ -0,0 +1,14 @@
+function fact(num) {
+    //number is 3 => 3 * 2 * 1 = 6
+    //we want to get all numbers including and less than and mu;tiply them together
+    //loop through it
+  
+    let smallerNumbersArray = [];
+    for (let i = 1; i <= num; i++) {
+      smallerNumbersArray.push(i)
+    }
+  
+     return smallerNumbersArray.reduce( (a,b) => a * b )
+  }
+  
+  console.log(fact(5))
