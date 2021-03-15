1eab133ec57a79f1feda86e613df9d72a1c4d743
JakesProjects1 <jacob@kagon.net>
2021-03-15 16:43:41 -0400

    done with recursive solution to factorial problem

--- factorial.js
+++ factorial.js
@@ -1,14 +1,24 @@
 function fact(num) {
-    //number is 3 => 3 * 2 * 1 = 6
-    //we want to get all numbers including and less than and mu;tiply them together
-    //loop through it
-  
-    let smallerNumbersArray = [];
-    for (let i = 1; i <= num; i++) {
-      smallerNumbersArray.push(i)
-    }
-  
-     return smallerNumbersArray.reduce( (a,b) => a * b )
+  //number is 3 => 3 * 2 * 1 = 6
+  //we want to get all numbers including and less than and mu;tiply them together
+  //loop through it
+
+  let smallerNumbersArray = [];
+  for (let i = 1; i <= num; i++) {
+    smallerNumbersArray.push(i);
   }
-  
-  console.log(fact(5))
+
+  return smallerNumbersArray.reduce((a, b) => a * b);
+}
+
+console.log(fact(5));
+
+//recursive solution
+
+function fact(num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  return n * fact(n - 1);
+}
