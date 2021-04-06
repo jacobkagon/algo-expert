751f67a87b14345ad3c127bb28eb02065fbe77a2
JakesProjects1 <jacob@kagon.net>
2021-04-06 16:52:49 -0700

    done with move to end

--- move-to-end.js
+++ move-to-end.js
@@ -1,14 +1,23 @@
 function moveElementToEnd(array, toMove) {
-	//check if the toMove number is in the array.
-	//add it to a new array
-  // Write your code here.
-	
-	let newArr = [];
-	for(let i = 0; i < array.length; i++) {
-		if(array[i] === toMove) {
-			newArr.push(array[i]);
-			
-		}
-	}
-	return array.filter((num) => num !== toMove).concat(newArr)
-}
+	// Write your code here.
+	  let i = 0;
+	  let j = array.length -1;
+	  
+	  while(i < j) {
+		  while(i < j && array[j] === toMove) {
+			  j--
+		  }
+		  if(array[i] === toMove ) {
+			  swap(i, j, array);
+		  }
+	  i++;
+	  }
+	  return array;
+  }
+  
+  function swap(i, j, array) {
+	  let temp = array[j];
+	   array[j] = array[i];
+	  array[i] = temp;
+  }
+  
