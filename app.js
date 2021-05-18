8cb8f1745d3d86a56982c90e1147d43d5902a63e
JakesProjects1 <jacob@kagon.net>
2021-05-18 15:50:42 -0400

    done with code challenge

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// let inputForm = document.querySelector(`#form`);
 
+// if (inputForm) {
+//   inputForm.addEventListener("submit", (event) => {
+//     handleSubmit(event);
+//   });
+// }
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   console.log(event);
+// }
+
+console.log(isAnagram("hare", "hear"));
